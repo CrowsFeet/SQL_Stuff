fe3da25caea0624f345a32e2363f79f14659c409
AG <36303884+CrowsFeet@users.noreply.github.com>
2018-06-26 10:54:30 +0100

    Update ChkCachePlans.sql

--- ChkCachePlans.sql
+++ ChkCachePlans.sql
@@ -1,3 +1,4 @@
+-- a few SQL commands for reviewing SQL Cache.
 -- The following example returns the SQL text of all cached entries that have been used more than once.
 SELECT usecounts, cacheobjtype, objtype, text 
 FROM sys.dm_exec_cached_plans 
@@ -64,11 +65,11 @@ SELECT objtype AS [CacheType]
         , count_big(*) AS [Total Plans]
         , sum(cast(size_in_bytes as decimal(18,2)))/1024/1024 AS [Total MBs]
         , avg(usecounts) AS [Avg Use Count]
-        , sum(cast((CASE WHEN usecounts = 1 THEN size_in_bytes ELSE 0 END) as decimal(18,2)))/1024/1024 AS [Total MBs – USE Count 1]
-        , sum(CASE WHEN usecounts = 1 THEN 1 ELSE 0 END) AS [Total Plans – USE Count 1]
+        , sum(cast((CASE WHEN usecounts = 1 THEN size_in_bytes ELSE 0 END) as decimal(18,2)))/1024/1024 AS [Total MBs â€“ USE Count 1]
+        , sum(CASE WHEN usecounts = 1 THEN 1 ELSE 0 END) AS [Total Plans â€“ USE Count 1]
 FROM sys.dm_exec_cached_plans
 GROUP BY objtype
-ORDER BY [Total MBs – USE Count 1] DESC
+ORDER BY [Total MBs â€“ USE Count 1] DESC
 GO 
 
 --------------------------------------------------------------------------------------

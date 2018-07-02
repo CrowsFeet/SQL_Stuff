f9ba562f130f9c32c1641869b95861c58e6198bd
AG <36303884+CrowsFeet@users.noreply.github.com>
2018-07-02 16:14:41 +0100

    Update CycleErrorLog.sql

--- CycleErrorLog.sql
+++ CycleErrorLog.sql
@@ -1,2 +1,7 @@
+-- I turn this into a SQL Server Agent normally as you want to run this once a week at least.
+-- In several roles that I have worked, I have encountered SQL Servers that have never had this cleared down.
+-- it can become a problem if you try to review this. so cycling this regularly will help keep it under control.
+
+
 -- cycle the error log
 -- sp_cycle_errorlog

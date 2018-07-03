1443a574786ccb96f5480faa215b9a06a1e18dff
AG <36303884+CrowsFeet@users.noreply.github.com>
2018-07-03 11:04:27 +0100

    Update Fnd_Locking.sql

--- Fnd_Locking.sql
+++ Fnd_Locking.sql
@@ -1,3 +1,4 @@
+-- not a good one.
 DECLARE @Handle varbinary(50), @Spid int = <your spid value/>
 SELECT @Handle = sql_handle from master.dbo.sysprocesses where spid = @spid
 DBCC INPUTBUFFER (@spid)
